497728f2ffac56b3596adb25d5bd9b9cfca912ed
rgbronze <91555798+rgbronze@users.noreply.github.com>
2021-09-29 15:03:31 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+# This is a Python comment in my fisrt Python app
+# This variable contains an integer
+quantity = 14
+# This variable contains a float
+unit_price = 26.99
+# This variable conatains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+# Show some results on the screen
+print(extended_price)
